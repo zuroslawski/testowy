b15d48094196634e5f8c040ad5aa57f57198ba98
zuroslawski <Bartlomiej.Zurek@deutschebahn.com>
2023-03-07 17:58:14 +0100

    Create files encrypt and decrypt file

---
+++ zad_encrypt.js
@@ -0,0 +1,25 @@
+const { pipeline } = require('stream').promises;
+const { createWriteStream, createReadStream } = require('fs');
+const {
+  createCipheriv, createDecipheriv, createCipher, createDecipher,
+} = require('crypto');
+const { promisify } = require('util');
+const { SALT } = require('./constants');
+const scrypt = promisify(require('crypto').scrypt);
+const randomBytes = promisify(require('crypto').randomBytes);
+
+(async () => {
+  const [,, inputFile, outputFile, pwd] = process.argv;
+  const algorythm = 'aes-192-cbc';
+
+  const key = await scrypt(pwd, SALT, 24);
+
+  const iv = await randomBytes(16);
+
+  await pipeline(
+    createReadStream(inputFile),
+    createCipher(algorythm, key),
+    createWriteStream(outputFile),
+  );
+  console.log('Done.');
+})();
